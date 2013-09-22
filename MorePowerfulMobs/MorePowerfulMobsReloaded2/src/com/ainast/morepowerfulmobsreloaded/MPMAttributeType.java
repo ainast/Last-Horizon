53e123daf7d8e8d1449f2d30c980268c1477861b
jaredtorrez <jaredtorrez@192.168.1.4>
2013-09-22 15:49:52 -0500

    Messed up on increased modifiability, this is the fix.

--- MorePowerfulMobs/MorePowerfulMobsReloaded2/src/com/ainast/morepowerfulmobsreloaded/MPMAttributeType.java
+++ MorePowerfulMobs/MorePowerfulMobsReloaded2/src/com/ainast/morepowerfulmobsreloaded/MPMAttributeType.java
@@ -1,50 +1,52 @@
 package com.ainast.morepowerfulmobsreloaded;
 
 public class MPMAttributeType {
-	final static String RESISTANCE_TO_DARK = MPMAttributeType.RESISTANCE_TO_DARK;
-	final static String RESISTANCE_TO_EARTH = MPMAttributeType.RESISTANCE_TO_EARTH;
-	final static String RESISTANCE_TO_FIRE = MPMAttributeType.RESISTANCE_TO_FIRE;
-	final static String RESISTANCE_TO_FORCE = MPMAttributeType.RESISTANCE_TO_FORCE;
-	final static String RESISTANCE_TO_ICE = MPMAttributeType.RESISTANCE_TO_ICE;
-	final static String RESISTANCE_TO_LIGHT = MPMAttributeType.RESISTANCE_TO_LIGHT;
-	final static String RESISTANCE_TO_LIGHTNING = MPMAttributeType.RESISTANCE_TO_LIGHTNING;
-	final static String RESISTANCE_TO_PHYSICAL = MPMAttributeType.RESISTANCE_TO_PHYSICAL;
-	final static String RESISTANCE_TO_MAGIC = MPMAttributeType.RESISTANCE_TO_MAGIC;
 	
-	final static String REDUCE_COOLDOWN_ON_JUMP = MPMAttributeType.REDUCE_COOLDOWN_ON_JUMP;
+	final static String RESISTANCE_TO_DARK = "Resistance to DARK";
+	final static String RESISTANCE_TO_EARTH = "Resistance to EARTH";
+	final static String RESISTANCE_TO_FIRE = "Resistance to FIRE";
+	final static String RESISTANCE_TO_FORCE = "Resistance to FORCE";
+	final static String RESISTANCE_TO_ICE = "Resistance to ICE";
+	final static String RESISTANCE_TO_LIGHT = "Resistance to LIGHT";
+	final static String RESISTANCE_TO_LIGHTNING = "Resistance to LIGHTNING";
+	final static String RESISTANCE_TO_PHYSICAL = "Resistance to PHYSICAL";
+	final static String RESISTANCE_TO_MAGIC = "Resistance to MAGIC";
 	
-	final static String MAXIMUM_MANA = MPMAttributeType.MAXIMUM_MANA;
-	final static String MAXIMUM_HEALTH = MPMAttributeType.MAXIMUM_HEALTH;
+	final static String REDUCE_COOLDOWN_ON_JUMP = "Reduce cooldown on JUMP";
 	
-	final static String HEALTH_REGENERATION = MPMAttributeType.HEALTH_REGENERATION;
-	final static String MANA_REGENERATION = MPMAttributeType.MANA_REGENERATION;
+	final static String MAXIMUM_MANA = "Maximum MANA";
+	final static String MAXIMUM_HEALTH = "Maximum HEALTH";
 	
-	final static String INCREASE_EXPERIENCE = MPMAttributeType.INCREASE_EXPERIENCE;
-	final static String DECREASE_EXPERIENCE = MPMAttributeType.DECREASE_EXPERIENCE;
-	final static String NEGATE_EXPERIENCE = MPMAttributeType.NEGATE_EXPERIENCE;
+	final static String HEALTH_REGENERATION = "HEALTH Regeneration";
+	final static String MANA_REGENERATION = "MANA Regeneration";
 	
-	final static String STRENGTH_1 = MPMAttributeType.STRENGTH_1;
-	final static String STRENGTH_2 = MPMAttributeType.STRENGTH_2;
-	final static String SPEED_1 = MPMAttributeType.SPEED_1;
-	final static String SPEED_2 = MPMAttributeType.SPEED_2;
-	final static String SLOW_1 = MPMAttributeType.SLOW_1;
-	final static String SLOW_2 = MPMAttributeType.SLOW_2;
-	final static String INVISIBILITY = MPMAttributeType.INVISIBILITY;
-	final static String FIRE_RESISTANCE_1 = MPMAttributeType.FIRE_RESISTANCE_1;
-	final static String FIRE_RESISTANCE_2 = MPMAttributeType.FIRE_RESISTANCE_2;
+	final static String INCREASE_EXPERIENCE = "Increase Experience";
+	final static String DECREASE_EXPERIENCE = "Decrease Experience";
+	final static String NEGATE_EXPERIENCE = "Negate EXPERIENCE";
 	
-	final static String INCREASE_ATTACK_DAMAGE = MPMAttributeType.INCREASE_ATTACK_DAMAGE;
-	final static String DECREASE_ATTACK_DAMAGE = MPMAttributeType.DECREASE_ATTACK_DAMAGE;
-	final static String INCREASE_MOVEMENT_SPEED = MPMAttributeType.INCREASE_MOVEMENT_SPEED;
-	final static String DECREASE_MOVEMENT_SPEED = MPMAttributeType.DECREASE_MOVEMENT_SPEED;
-	final static String INCREASE_KNOCKBACK_RESISTANCE = MPMAttributeType.INCREASE_KNOCKBACK_RESISTANCE;
-	final static String DECREASE_KNOCKBACK_RESISTANCE = MPMAttributeType.DECREASE_KNOCKBACK_RESISTANCE;
+	final static String STRENGTH_1 = "STRENGTH I";
+	final static String STRENGTH_2 = "STRENGTH II";
+	final static String SPEED_1 = "SPEED I";
+	final static String SPEED_2 = "SPEED II";
+	final static String SLOW_1 = "SLOW I";
+	final static String SLOW_2 = "SLOW II";
+	final static String INVISIBILITY = "INVISIBILITY";
+	final static String FIRE_RESISTANCE_1 = "RESISTANCE TO FIRE I";
+	final static String FIRE_RESISTANCE_2 = "RESISTANCE TO FIRE II";
 	
-	final static String DEATH_DEFYING = MPMAttributeType.DEATH_DEFYING;
-	final static String DEVILS_TAKE = MPMAttributeType.DEVILS_TAKE;
-	final static String MITCHIRINEKO_MARCH = MPMAttributeType.MITCHIRINEKO_MARCH;
+	final static String INCREASE_ATTACK_DAMAGE = "Increase ATTACK DAMAGE";
+	final static String DECREASE_ATTACK_DAMAGE = "Decrease ATTACK DAMAGE";
+	final static String INCREASE_MOVEMENT_SPEED = "Increase MOVEMENT SPEED";
+	final static String DECREASE_MOVEMENT_SPEED = "Decrease MOVEMENT SPEED";
+	final static String INCREASE_KNOCKBACK_RESISTANCE = "Increase KNOCKBACK RESISTANCE";
+	final static String DECREASE_KNOCKBACK_RESISTANCE = "Decrease KNOCKBACK RESISTANCE";
 	
-	final static String POISONOUS = MPMAttributeType.POISONOUS;
-	final static String BLINDING = MPMAttributeType.BLINDING;
-	final static String HEALING = MPMAttributeType.HEALING;
+	final static String DEATH_DEFYING = "DEATH DEFYING";
+	final static String DEVILS_TAKE = "DEVILS TAKE";
+	final static String MITCHIRINEKO_MARCH = "Mitchirineko March";
+	
+	final static String POISONOUS = "Poisonous";
+	final static String BLINDING = "Blinding";
+	final static String HEALING = "Healing";
+	
 }
