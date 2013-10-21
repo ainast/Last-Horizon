930d2d4c8404c2b0075ac3c5f79c795eb15f3377
jaredtorrez <jaredtorrez@glamdring>
2013-10-20 20:02:50 -0500

    Made testbow a 100% drop.

--- MorePowerfulMobs/MorePowerfulMobsReloaded2/src/com/ainast/morepowerfulmobsreloaded/MPMMobTypes.java
+++ MorePowerfulMobs/MorePowerfulMobsReloaded2/src/com/ainast/morepowerfulmobsreloaded/MPMMobTypes.java
@@ -44,7 +44,6 @@ public class MPMMobTypes {
 	
 	public static ItemStack getCorruptedGuardDrop() {
 		ItemStack[] dropTable = { //add items seperated by comma
-								TieredItems.getTier1PristinePlateGreaves(),
 								TieredItems.getTestBow()
 								};
 		int i = MPMTools.generator.nextInt(dropTable.length);
